a02fd4aaa9ab783bd1429a6b4ee3e714222c29a2
Joris Verstegen <39861914+BigInteger28@users.noreply.github.com>
2024-03-25 13:37:33 +0100

    Update spin.go

--- spin.go
+++ spin.go
@@ -10,29 +10,38 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	for {
-		fmt.Print("\nSPIN THE WHEEL: ")
-		fmt.Scanln()
-		file, err := os.Open("keuzes.txt") // Vervang "keuzes.txt" met het pad naar je bestand
-		if err != nil {
-			fmt.Println("Fout bij het openen van het bestand:", err)
-			return
+	var bestand string = "keuzes"
+	for {		
+		var keuze string
+		fmt.Println("1. BESTAND KIEZEN")
+		fmt.Println("SPIN THE WHEEL")
+		fmt.Print(": ")
+		fmt.Scanln(&keuze)
+		if keuze == "1" {
+			fmt.Print("Ik kies bestand: ")
+			fmt.Scanln(&bestand)
+		} else {			
+			file, err := os.Open(bestand+".txt")
+			if err != nil {
+				fmt.Println("Fout bij het openen van het bestand:", err)
+				return
+			}
+			defer file.Close()
+			var keuzes []string
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				keuzes = append(keuzes, scanner.Text())
+			}
+			if err := scanner.Err(); err != nil {			
+				fmt.Println("Fout bij het lezen van het bestand:", err)
+				return
+			}
+			if len(keuzes) > 0 {
+			spin := rand.Intn(len(keuzes))
+			fmt.Println("\n",keuzes[spin])
+			} else {
+				fmt.Println("Geen keuzes gevonden in het bestand.")
+			}			
 		}
-		defer file.Close()
-		var keuzes []string
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			keuzes = append(keuzes, scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {			
-			fmt.Println("Fout bij het lezen van het bestand:", err)
-			return
-		}
-		if len(keuzes) > 0 {
-		spin := rand.Intn(len(keuzes))
-		fmt.Println("\n",keuzes[spin])
-		} else {
-			fmt.Println("Geen keuzes gevonden in het bestand.")
-		}			
 	}
 }
